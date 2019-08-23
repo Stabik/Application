20786ac8d6e20acba017395c22479011515903dd
Stabik <patriot88.rus@mail.ru>
2019-08-23 16:07:08 +0300

    2nd

---
+++ ClassLibrary.model/Folder.cs
@@ -0,0 +1,26 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.model
+{
+   public class Folder
+    {
+        public virtual int Id { get; set; }
+        public virtual string NameFolder { get; set; }
+        public virtual IList<Version> Version { get; set; }
+        public virtual RightAccess MyRightAccess { get; set; }
+    }
+    public class FolderMap : ClassMap<Folder>
+    {
+        public FolderMap()
+        {
+            Id(x => x.Id).GeneratedBy.HiLo("100");
+            Map(x => x.NameFolder).Length(50);
+            HasMany(x => x.Version).Inverse();
+            References(x => x.MyRightAccess).Cascade.SaveUpdate();
+
+        }
+    }
+}
---
+++ ClassLibrary.model/Groupe.cs
@@ -0,0 +1,33 @@
+﻿
+using ClassLibrary.model;
+using FluentNHibernate.Mapping;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.model
+{
+    public class Group
+    {
+        public virtual int Id { get; set; }
+        public virtual string NameGroup { get; set; }
+        public virtual RightAccess MyRightAccess { get; set; }
+
+        public virtual IList<Person> PersonList { get; set; }
+        public Group()
+        {
+            PersonList = new List<Person>();
+        }
+    }
+
+    public class GroupMap : ClassMap<Group>
+    {
+        public GroupMap()
+        {
+            Id(x => x.Id).GeneratedBy.HiLo("100");
+            Map(x => x.NameGroup).Length(50);
+            HasMany(x => x.PersonList).Inverse();
+            References(x => x.MyRightAccess).Cascade.SaveUpdate();
+        }
+    }
+}
+
